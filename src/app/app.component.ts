543dc4bf5f2a6d31939b4978457521035dbbbc0c
Ramzan Ali Khan <frontendarea@gmail.com>
2023-09-30 18:58:54 +0530

    layout fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,6 @@ export class AppComponent {
     cities: City[] | undefined;
 
     selectedCity: City | undefined;
-    
    
     images: any[] | undefined;
 
@@ -47,82 +46,34 @@ export class AppComponent {
             { name: 'Paris', code: 'PRS' }
         ];
 
-       this.images=[
+        this.images=[
         {
-            itemImageSrc: 'https://alahazrat.net/wp-content/uploads/2018/10/slider.png',
+            itemImageSrc: '.././assets//images/slider/Slider-RabiulAwal.jpg',
             alt: 'Description for Image 1',
             title: 'Title 1'
         },
         {
-            itemImageSrc: 'https://alahazrat.net/wp-content/uploads/2018/11/urs-e-ala-hazrat-alahazrat.png',
-            alt: 'Description for Image 2',
+            itemImageSrc: '.././assets//images/slider/jumma-durood-e-razwi.jpg',
+            alt: 'Description for Image 1',
             title: 'Title 2'
         },
         {
-            itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria3.jpg',
-            alt: 'Description for Image 3',
+            itemImageSrc: '.././assets//images/slider/jumma-surah-yaseen-2.jpg',
+            alt: 'Description for Image 1',
             title: 'Title 3'
         },
         {
-            itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria4.jpg',
-            alt: 'Description for Image 4',
+            itemImageSrc: '.././assets//images/slider/eid-namaz-tareeqa.jpg',
+            alt: 'Description for Image 1',
             title: 'Title 4'
         },
         {
-            itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria5.jpg',
-            alt: 'Description for Image 5',
+            itemImageSrc: '.././assets//images/slider/wednesday-dua.jpg',
+            alt: 'Description for Image 1',
             title: 'Title 5'
-        },
-        {
-            itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria6.jpg',
-            alt: 'Description for Image 6',
-            title: 'Title 6'
-        },
-        {
-            itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria7.jpg',
-            alt: 'Description for Image 7',
-            title: 'Title 7'
-        },
-        {
-            itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria8.jpg',
-            alt: 'Description for Image 8',
-            title: 'Title 8'
-        },
-        {
-            itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria9.jpg',
-            alt: 'Description for Image 9',
-            title: 'Title 9'
-        },
-        {
-            itemImageSrc: 'https://alahazrat.net/wp-content/uploads/2020/12/wednesday-dua.jpg',
-            alt: 'Description for Image 10',
-            title: 'Title 10'
-        },
-        {
-            itemImageSrc: 'https://primefaces.org/cdn/primeng/images/galleria/galleria11.jpg',
-            alt: 'Description for Image 11',
-            title: 'Title 11'
-        },
-        {
-            itemImageSrc: 'https://alahazrat.net/wp-content/uploads/2020/12/wednesday-dua.jpg',
-            alt: 'Description for Image 12',
-            title: 'Title 12'
-        },
-        {
-            itemImageSrc: 'https://alahazrat.net/wp-content/uploads/2020/12/monday-thursday-roza.jpg',
-            alt: 'Description for Image 13',
-            title: 'Title 13'
-        },
-        {
-            itemImageSrc: 'https://alahazrat.net/wp-content/uploads/2020/12/monday-thursday-roza.jpg',
-            alt: 'Description for Image 14',
-            title: 'Title 14'
-        },
-        {
-            itemImageSrc: 'https://alahazrat.net/wp-content/uploads/2020/12/jumma-durood-e-razwi.jpg',
-            alt: 'Description for Image 15',
-            title: 'Title 15'
         }
+
+      
        ]
     }
     
